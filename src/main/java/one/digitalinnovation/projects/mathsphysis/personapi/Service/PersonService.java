58fe35d4b4ebb2f161b9b3cd867f2ada68ec905c
Maths Physis <smathsphysis@gmail.com>
2021-07-29 21:00:14 -0400

    feat: Added Get endpoint and Read operation

--- src/main/java/one/digitalinnovation/projects/mathsphysis/personapi/Service/PersonService.java
+++ src/main/java/one/digitalinnovation/projects/mathsphysis/personapi/Service/PersonService.java
@@ -8,19 +8,21 @@ import one.digitalinnovation.projects.mathsphysis.personapi.repository.PersonRep
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class PersonService {
 
-    private PersonRepository personRepository;
-
     private final PersonMapper personMapper = PersonMapper.INSTANCE;
+    private PersonRepository personRepository;
 
     @Autowired
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
-    public MessageResponseDTO createPerson(PersonDTO personDTO){
+    public MessageResponseDTO createPerson(PersonDTO personDTO) {
         Person personToSave = personMapper.toModel(personDTO);
 
         Person savedPerson = personRepository.save(personToSave);
@@ -28,5 +30,14 @@ public class PersonService {
                 .builder()
                 .message("Created Person with ID: " + savedPerson.getId())
                 .build();
+    }
+
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/one/digitalinnovation/projects/mathsphysis/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/projects/mathsphysis/personapi/controller/PersonController.java
@@ -4,12 +4,12 @@ package one.digitalinnovation.projects.mathsphysis.personapi.controller;
 import one.digitalinnovation.projects.mathsphysis.personapi.Service.PersonService;
 import one.digitalinnovation.projects.mathsphysis.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.projects.mathsphysis.personapi.dto.response.MessageResponseDTO;
-import one.digitalinnovation.projects.mathsphysis.personapi.entity.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
@@ -26,5 +26,10 @@ public class PersonController {
     @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
         return personService.createPerson(personDTO);
+    }
+
+    @GetMapping
+    public List<PersonDTO> listAll() {
+        return personService.listAll();
     }
 }
